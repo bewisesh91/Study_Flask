075f92e3d0d9cf6d69fc13300274753b1be6e5e7
bewisesh91 <bewise.seunghyun@gmail.com>
2021-02-10 14:44:44 +0900

    flask

--- Practice/mynote/script.js
+++ Practice/mynote/script.js
@@ -1,3 +1,16 @@
-function hey() {
-    alert('안녕')
+// $(document).ready(function(){
+// //    페이지가 로딩되면 실행하고 싶은 함수 작성
+// });
+
+function openClose() {
+    let status = $('#main-posting-box').css('display');
+    if (status == 'block') {
+        $('#main-posting-box').hide();
+        $('#main-btn').text('기록하기');
+    } else {
+        $('#main-posting-box').show();
+        $('#main-btn').text('기록닫기');
+    }
 }
+
+
